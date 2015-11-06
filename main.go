5b06a7ea0bdc408cbd9cdacbb252ef5f4dc785a0
MohamedBassem <medoox240@gmail.com>
2015-11-06 15:58:02 +0200

    A working commandline

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"io"
 	"log"
+	"mime/multipart"
 	"net/http"
+	"os"
 	"os/exec"
+	"strconv"
 	"strings"
 	"time"
 
@@ -22,21 +25,29 @@ func pinger(ws *websocket.Conn) {
 }
 
 func runCommand(ws *websocket.Conn, jobID, command string) {
-	cmd := exec.Command("ls", "-l")
+	s := strings.Split(command, " ")
+	websocket.Message.Send(ws, "$ "+command+"\n")
+	cmd := exec.Command(s[0], s[1:]...)
 	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
 	defer stdout.Close()
-	if err != nil {
-		log.Println(err.Error())
-		return
-	}
-	stderr, err := cmd.StderrPipe()
-	defer stderr.Close()
-	if err != nil {
-		log.Println(err.Error())
-		return
-	}
 
-	cmd.Start()
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+	defer stderr.Close()
+
+	err = cmd.Start()
+	defer websocket.Message.Send(ws, "\n")
+	if err != nil {
+		websocket.Message.Send(ws, err.Error()+"\n")
+		return
+	}
 	defer cmd.Wait()
 
 	go io.Copy(ws, stdout)
@@ -65,8 +76,44 @@ func handleRun(ws *websocket.Conn) {
 
 }
 
+func handleUpload(w http.ResponseWriter, r *http.Request) {
+
+	const _24K = (1 << 20) * 24
+	r.ParseMultipartForm(_24K)
+
+	if r.FormValue("task-id") == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	var err error
+	for _, fheaders := range r.MultipartForm.File {
+		for _, hdr := range fheaders {
+			// open uploaded
+			var infile multipart.File
+			if infile, err = hdr.Open(); nil != err {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			// open destination
+			var outfile *os.File
+			if outfile, err = os.Create("/tmp/" + r.Form.Get("task-id") + "/output/" + hdr.Filename); nil != err {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			// 32K buffer copy
+			var written int64
+			if written, err = io.Copy(outfile, infile); nil != err {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			log.Println("uploaded file:" + hdr.Filename + ";length:" + strconv.Itoa(int(written)))
+		}
+	}
+}
+
 func main() {
 	http.Handle("/run", websocket.Handler(handleRun))
+	http.HandleFunc("/uploadcode", handleUpload)
 	log.Println("Starting server ..")
 	log.Fatal(http.ListenAndServe(":5055", nil))
 }
